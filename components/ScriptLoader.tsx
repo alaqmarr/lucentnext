b7448b3d80bf40c564ffb5f185df3bd0df2b9ba4
Al Aqmar <alaqmarak0810@gmail.com>
2025-09-02 14:12:20 +0530

    deploy

--- components/ScriptLoader.tsx
+++ components/ScriptLoader.tsx
@@ -1,39 +1,69 @@
 'use client';
 
-import Script from 'next/script';
+const scriptLoader = () => {
+  const html = `
+  <script>
+$('.main--content .fixcontent').theiaStickySidebar({
+additionalMarginTop: 100
+});
+</script>
 
-const ScriptLoader = () => {
+<script>
+    const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach((accordion, index) => {
+  const header = accordion.querySelector(".accordion__header");
+  const content = accordion.querySelector(".accordion__content");
+  const icon = accordion.querySelector("#accordion-icon");
+
+  header.addEventListener("click", () => {
+    const isOpen = content.style.height === content.scrollHeight + "px";
+
+    accordions.forEach((a, i) => {
+      const c = a.querySelector(".accordion__content");
+      const ic = a.querySelector("#accordion-icon");
+
+      c.style.height = i === index && !isOpen ? c.scrollHeight + "px" : "0px";
+      ic.classList.toggle("ri-add-line", i !== index || !isOpen);
+      ic.classList.toggle("ri-subtract-fill", i === index && !isOpen);
+    });
+  });
+});
+
+</script>
+  `
   return (
     <>
-      <Script src="/assets/vendors/jquery/jquery-3.6.0.min.js" strategy="beforeInteractive" />
-      <Script src="/assets/vendors/bootstrap/js/bootstrap.bundle.min.js" strategy="beforeInteractive" />
-      <Script src="/assets/vendors/jarallax/jarallax.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-appear/jquery.appear.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-validate/jquery.validate.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/odometer/odometer.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/swiper/swiper.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/wnumb/wNumb.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/wow/wow.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/isotope/isotope.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/owl-carousel/owl.carousel.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/bootstrap-select/js/bootstrap-select.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery-ui/jquery-ui.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery.circle-type/jquery.circleType.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/jquery.circle-type/jquery.lettering.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/nice-select/jquery.nice-select.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/marquee/marquee.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/gsap/gsap.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/gsap/ScrollTrigger.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/gsap/SplitText.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/scroll/scrolltop.min.js" strategy="afterInteractive" />
-      <Script src="/assets/vendors/scroll/lenis.min.js" strategy="afterInteractive" />
-      <Script src="/assets/js/elitecons.js" strategy="afterInteractive" />
-      <Script src="/assets/js/theia-sticky-sidebar.min.js" strategy="afterInteractive" />
+      <script src="/assets/vendors/jquery/jquery-3.6.0.min.js" />
+      <script src="/assets/vendors/bootstrap/js/bootstrap.bundle.min.js" />
+      <script src="/assets/vendors/jarallax/jarallax.min.js" />
+      <script src="/assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js" />
+      <script src="/assets/vendors/jquery-appear/jquery.appear.min.js" />
+      <script src="/assets/vendors/jquery-circle-progress/jquery.circle-progress.min.js" />
+      <script src="/assets/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js" />
+      <script src="/assets/vendors/jquery-validate/jquery.validate.min.js" />
+      <script src="/assets/vendors/odometer/odometer.min.js" />
+      <script src="/assets/vendors/swiper/swiper.min.js" />
+      <script src="/assets/vendors/wnumb/wNumb.min.js" />
+      <script src="/assets/vendors/wow/wow.js" />
+      <script src="/assets/vendors/isotope/isotope.js" />
+      <script src="/assets/vendors/owl-carousel/owl.carousel.min.js" />
+      <script src="/assets/vendors/bootstrap-select/js/bootstrap-select.min.js" />
+      <script src="/assets/vendors/jquery-ui/jquery-ui.js" />
+      <script src="/assets/vendors/jquery.circle-type/jquery.circleType.js" />
+      <script src="/assets/vendors/jquery.circle-type/jquery.lettering.min.js" />
+      <script src="/assets/vendors/nice-select/jquery.nice-select.min.js" />
+      <script src="/assets/vendors/marquee/marquee.min.js" />
+      <script src="/assets/vendors/gsap/gsap.js" />
+      <script src="/assets/vendors/gsap/ScrollTrigger.js" />
+      <script src="/assets/vendors/gsap/SplitText.js" />
+      <script src="/assets/vendors/scroll/scrolltop.min.js" />
+      <script src="/assets/vendors/scroll/lenis.min.js" />
+      <script src="/assets/js/elitecons.js" />
+      <script src="/assets/js/theia-sticky-sidebar.min.js" />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 };
 
-export default ScriptLoader;
+export default scriptLoader;
